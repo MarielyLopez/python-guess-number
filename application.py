316f548d994faa79da982737ed7b37f16090bd7c
MarielyLopez <mariielylopez795@gmail.com>
2015-05-08 16:12:25 -0600

    Funciones Principales

--- application.py
+++ application.py
@@ -1,3 +1,16 @@
 # Aqui escribe tu codigo
 
-print "Hello World"
+import random
+
+print " "
+print "Welcome, we will play Guess a Number "
+print " "
+number_random = random.randint(1,20)
+print number_random
+guess = int(raw_input ("Insert a Number: "))
+if guess == number_random:
+	print "You win!" 
+elif guess < number_random:
+	print "Wrong.Your number is very low. Try again "
+else:
+	print "Wrong.Your number is very high. Try again"
