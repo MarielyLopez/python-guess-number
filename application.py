923a91a6fb207244c86e44da9afed83150f1e46d
MarielyLopez <mariielylopez795@gmail.com>
2015-05-14 16:55:09 -0600

    Story Max Number of trials, made.

--- application.py
+++ application.py
@@ -7,32 +7,42 @@ print " "
 print "Welcome, we will play GUESS a Number "
 print " "
 
-COUNT = 0
+#COUNT = 0
 NEW_GAME = True
 
 while NEW_GAME == True:
     NUMBER_RANDOM = random.randint(1, 20)
-    for COUNT in range(1, 5):
+    TURN = 4
+    for COUNT in range(1, TURN+1):
         print NUMBER_RANDOM
         try:
-            GUESS = int(raw_input("Insert a Number: "))
-            if GUESS == NUMBER_RANDOM:
-                print "You win!"
-                break
-            elif GUESS < NUMBER_RANDOM:
-                print "Wrong.Your number is very low. Try again "
-            else:
-                print "Wrong.Your number is very high. Try again"
+            print "Turno numero: ",COUNT
+            if COUNT < 5:
+                GUESS = int(raw_input("Insert a Number: "))
+                while GUESS <0 or GUESS >= 20:
+                    #"Tyr"
+                    GUESS = int(raw_input("Insert a Number: "))
+                if GUESS == NUMBER_RANDOM:
+                    print "You win!"
+                    break
+                elif GUESS < NUMBER_RANDOM:
+                    if COUNT < 4:
+                        print "Wrong.Your number is very low."
+                elif GUESS > NUMBER_RANDOM:
+                    if COUNT < 4:
+                        print "Wrong.Your number is very high."
+                COUNT += 1
+            if COUNT == 5:
+                print "Game Over"
         except ValueError:
-            print "Enter only numbers"
-
+                print "Enter only numbers"
 
     ANSWER = True
     while ANSWER == True:
         print "Do you want to play again? y/n: "
         PLAY_AGAIN = raw_input('> ')
         PLAY_AGAIN = PLAY_AGAIN.lower()
-        if PLAY_AGAIN == "y":
+        if PLAY_AGAIN == "y": #
             ANSWER = False
             NEW_GAME = True
         elif PLAY_AGAIN == "n":
